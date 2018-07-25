a73b182d5efa57c54066298812502ebfeaea395f
Tommy Ku <zoo1929@outlook.com>
2018-07-25 13:50:13 +0800

    ADD: Update

--- pages/home.js
+++ pages/home.js
@@ -7,6 +7,7 @@ class PageHome extends HTMLElement {
     this.attachShadow({ mode: 'open' })
       .appendChild(template.cloneNode(true));
 
+    this.itemsDisplayed = 0;
   }
 
   connectedCallback() {
@@ -19,12 +20,28 @@ class PageHome extends HTMLElement {
       $ajaxRequest.reload()
         .then((response) => response.json())
         .then((json) => this.updateList(json))
+        .then(() => this.revealList())
         .then(() => this.hideLoader())
+        .then(() => this.registerInfiniteScroll())
         .catch((e) => this.updateListError(e));
     } else {
       const $appRouter = document.querySelector('app-router');
       $appRouter.go('/welcome');
     }
+  }
+
+  registerInfiniteScroll() {
+    // setup infinite scroll
+    const $infiniteFloat = this.shadowRoot.querySelector('main');
+    const $infinite = this.shadowRoot.querySelector('.infinite');
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.9
+    }
+
+    const observer = new IntersectionObserver(() => this.revealList(), options);
+    observer.observe($infinite);
   }
 
   hideLoader() {
@@ -33,16 +50,37 @@ class PageHome extends HTMLElement {
   }
 
   updateList(json) {
-    console.log(json);
     this.data = json;
+    this.entriesCount = this.data.feed.entry.length;
+  }
+
+  revealList(count) {
+    if (this.debounced) {
+      return;
+    } else {
+      this.debounced = true;
+    }
+
     const $list = this.shadowRoot.querySelector('.list');
-    json.feed.entry.forEach((item) => {
-      const $linkItem = document.createElement('link-item');
-      $linkItem.setAttribute('title', item['gsx$title']['$t']);
-      $linkItem.setAttribute('url', item['gsx$link']['$t']);
-      $linkItem.setAttribute('description', item['gsx$description']['$t']);
-      $list.appendChild($linkItem);
-    });
+    const itemsToDisplay = count || 20;
+    const endKey = Math.min(
+      Math.max(0, this.itemsDisplayed + itemsToDisplay - 1),
+      this.entriesCount - 1
+    );
+
+    this.data.feed.entry
+      .slice(this.itemsDisplayed, endKey)
+      .forEach((item, index) => {
+        const $linkItem = document.createElement('link-item');
+        $linkItem.setAttribute('title', item['gsx$title']['$t']);
+        $linkItem.setAttribute('url', item['gsx$link']['$t']);
+        $linkItem.setAttribute('description', item['gsx$description']['$t']);
+        $list.appendChild($linkItem);
+      });
+
+    this.itemsDisplayed += itemsToDisplay;
+
+    setTimeout(() => (this.debounced = false), 250);
   }
 
   updateListError(e) {
